9da693e9c3b2ce68648bcbff5234d1ff5a7cd7b0
sumeetg23 <sumeetg23@yahoo.com>
2016-06-22 07:35:04 -0400

    Add files via upload

--- FASTQ_Format_Conversion.pl
+++ FASTQ_Format_Conversion.pl
@@ -1,6 +1,14 @@
 #!/usr/bin/perl
 # Created by Sumeet Gupta (sgupta@wi.mit.edu) - 21st June 2016
 
+# Converts FASTQ read header/indentifier format
+
+# Automatically identifies the format of the read id and changes it between the 2 following 2 formats
+# read id format 1: @HWUSI-EAS100R:6:73:941:1973#0/1
+# read id format 2: @EAS139:136:FC706VJ:2:2104:15343:197393 1:Y:18:ATCACG
+
+# FASTQ files from WI Genome core end with ";1" or ";2" in format 1 specified above - This script can handle this variation
+
 use warnings;
 use strict;
 use Carp;
@@ -42,12 +50,13 @@ while(my $newline = <$PIPEIN>) {
 	}
 	
 	my $newid = changeidformat($id,$formatconv,$randomrunid);
-	
 	print $newid."\n";
 	
 	$newline = <$PIPEIN>;
 	print $newline;
 	$newline = <$PIPEIN>;
+	$newline =~ s/[\n\r]//g;
+	$newid = changeidformat($newline,$formatconv,$randomrunid);
 	print $newid."\n";
 	$newline = <$PIPEIN>;
 	print $newline;
